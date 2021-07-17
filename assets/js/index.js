77585bcf4c78e138eb623cfe8a8974c354b8db7c
MahdiyarGHD <m.gh.1385.mahdiyar@gmail.com>
2021-07-17 20:34:39 +0430

    Upload files

--- assets/js/index.js
+++ assets/js/index.js
@@ -111,20 +111,13 @@ if (detectMob()) {
 }
 
 function copyText(text) {
-
-    // Create new element
     var el = document.createElement('textarea');
-    // Set value (string to be copied)
     el.value = text;
-    // Set non-editable to avoid focus and move outside of view
     el.setAttribute('readonly', '');
     el.style = { position: 'absolute', left: '-9999px' };
     document.body.appendChild(el);
-    // Select text inside element
     el.select();
-    // Copy text to clipboard
     document.execCommand('copy');
-    // Remove temporary element
     document.body.removeChild(el);
 
 }
@@ -212,3 +205,4 @@ function rightClick(e) {
 hljs.highlightAll();
 ScrollReveal({ reset: true ,delay:50});
 ScrollReveal().reveal('.object');
+ScrollReveal().reveal('.header');
