18db34404f54ba4f6577bc71cfd85603e03d1ecc
MahdiyarGHD <m.gh.1385.mahdiyar@gmail.com>
2024-07-03 22:11:43 +0330

    fix: Fix repo link on star btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
                         <span className='icon-moon block mt-[3px] text-white/90'></span>
                     </div>
                     <div className='float-right h-6 w-[0.5px] bg-gray-200/10 mr-3 mt-[15px]'></div> */}
-                    <a href="https://github.com/laravel-cheat-sheet/" target="_blank" className='group float-right mr-4 mt-[14px] w-20 h-[25px] text-[11px] text-center rounded-3xl text-white/70 bg-white/[3%] rounded-[23px] shadow-md backdrop-blur-3xl border-white/5 border hover:bg-white/[10%] cursor-pointer'>
+                    <a href="https://github.com/MahdiyarGHD/laravel-cheat-sheet/" target="_blank" className='group float-right mr-4 mt-[14px] w-20 h-[25px] text-[11px] text-center rounded-3xl text-white/70 bg-white/[3%] rounded-[23px] shadow-md backdrop-blur-3xl border-white/5 border hover:bg-white/[10%] cursor-pointer'>
                         <div className='float-right mr-[8px] mt-[3px]'>ستاره بده </div><span className='group-hover:text-yellow-300 text-white text-[13px] icon-star-empty mt-[5px] float-right mr-[5px]'></span>
                     </a>
                 </div>
